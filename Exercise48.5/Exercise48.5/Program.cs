a3b428f6e5aee11aa1d731305017f8a17ecddf89
ribex <ribexy@gmail.com>
2017-11-19 15:19:22 -0500

    Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.

---
+++ Exercise48.5/Exercise48.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise48._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers separated by commas: ");
+            var numbers = Console.ReadLine().Split(',');
+
+            var max = Int32.MinValue;
+            foreach (var number in numbers)
+            {
+                max = (max > Convert.ToInt32(number)) ? max : Convert.ToInt32(number);
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
