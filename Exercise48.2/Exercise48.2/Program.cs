f81cd9667cfce175ba1d582ff305e77169c24df8
ribex <ribexy@gmail.com>
2017-11-19 14:52:30 -0500

    Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.

---
+++ Exercise48.2/Exercise48.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise48._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            string entry = "";
+
+            do
+            {
+                Console.WriteLine("Enter a number, or ok to exit: ");
+                entry = Console.ReadLine();
+                if (entry !=  "ok")
+                {
+                    sum += Convert.ToInt32(entry);
+                }
+            } while (entry != "ok");
+
+            Console.WriteLine("The sum is " + sum);
+        }
+    }
+}
