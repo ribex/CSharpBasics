8c798b7d332775017f534bb2722aac363d837d93
ribex <ribexy@gmail.com>
2017-11-19 16:59:48 -0500

    Examples of List methods

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demonstration of List methods
+
+            var numbers = new List<int>() {1, 2, 3, 4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5, 6, 7});
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1)); // without overload, gives first index at which item is found
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            //numbers.Remove(1);
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            // cannot use foreach for removing
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
+        }
+    }
+}
