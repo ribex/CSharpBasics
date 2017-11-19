d53b3e3f54d876ede7d37a038e48761e0b7b0df2
ribex <ribexy@gmail.com>
2017-11-19 12:59:14 -0500

    Exercise 2: Write a program which takes two numbers from the console and displays the maximum of the two.

---
+++ Exercises42.2/Exercises42.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises42._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers:");
+            var num1 = Int32.Parse(Console.ReadLine());
+            var num2 = Int32.Parse(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine("The maximum value is " + num1);
+            }
+            else
+            {
+                Console.WriteLine("The maximum value is " + num2);
+            }
+        }
+    }
+}
