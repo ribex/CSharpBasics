90326537657bcc0bfd9646bc1494116cf105ba30
ribex <ribexy@gmail.com>
2017-11-19 13:02:52 -0500

    Exercise 3: Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.

---
+++ Exercise42.3/Exercise42.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of the image: ");
+            var width = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height of the image: ");
+            var height = Int32.Parse(Console.ReadLine());
+            if (height > width)
+            {
+                Console.WriteLine("The image is portrait.");
+            }
+            else
+            {
+                Console.WriteLine("The image is landscape.");
+            }
+        }
+    }
+}
