cf0784b0e7a737bdf740ba2ee079fd0dd17e049b
ribex <ribexy@gmail.com>
2017-11-19 12:29:22 -0500

    Example using ternary operator

--- Conditionals/Conditionals/Program.cs
+++ Conditionals/Conditionals/Program.cs
@@ -10,20 +10,22 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int hour = 10;
+            bool isGoldCustomer = true;
 
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning.");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon.");
-            }
-            else
-            {
-                Console.WriteLine("It's evening.");
-            }
+            //float price;
+            //if (isGoldCustomer)
+            //{
+            //    price = 19.95f;
+            //}
+            //else
+            //{
+            //    price = 29.95f;
+            //}
+
+            // ternary operator
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
         }
     }
 }
