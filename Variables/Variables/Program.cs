1edda20d6dc6a129bca065d941fe9fea6f4a2f42
ribex <ribexy@gmail.com>
2017-11-19 09:05:22 -0500

    Example with a constant and illegal reassignment

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,8 +6,10 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+
+            /* This is not allowed */
+            Pi = 1;
         }
     }
 }
