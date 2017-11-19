2b72555bfaf36892b867f20bbc0dd1ecfc2af821
ribex <ribexy@gmail.com>
2017-11-19 12:07:35 -0500

    Examples of methods regarding value and reference

---
+++ ReferenceTypesAndValueTypes2/ReferenceTypesAndValueTypes2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypesAndValueTypes2
+{
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number); // this 'number' is a different location in memory than 'number' in previous line
+            Console.WriteLine(number); // 1
+
+            var person = new Person() {Age = 20}; 
+            MakeOld(person); // same object as in previous line 
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
