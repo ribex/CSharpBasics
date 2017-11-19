cbaa99cf681b85f90fd78481689063f34ae877e6
ribex <ribexy@gmail.com>
2017-11-19 10:10:59 -0500

    Use Resharper to move Person class to its own class file

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -1,17 +1,5 @@
-﻿using System;
-
-namespace Classes
+﻿namespace Classes
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
