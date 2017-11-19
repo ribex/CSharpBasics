0593c71d33c10a0a733658c42dda274b7e45e86c
ribex <ribexy@gmail.com>
2017-11-19 14:37:18 -0500

    Exercise 1:  Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.

---
+++ Exercise48.1/Exercise48.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise48._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("There are " + count + " numbers between 1 and 100 that are evenly divisible by 3.");
+        }
+    }
+}
