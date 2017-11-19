3573cec1f2a6ab927ee56811cdd8e900db816573
ribex <ribexy@gmail.com>
2017-11-19 12:39:18 -0500

    Example with enum and switch

--- Conditionals/Conditionals/Program.cs
+++ Conditionals/Conditionals/Program.cs
@@ -10,22 +10,23 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
+            var season = Season.Autumn;
 
-            //float price;
-            //if (isGoldCustomer)
-            //{
-            //    price = 19.95f;
-            //}
-            //else
-            //{
-            //    price = 29.95f;
-            //}
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Spring:
+                    Console.WriteLine("It's a beautiful season.");
+                    break;
 
-            // ternary operator
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+                case Season.Summer:
+                    Console.WriteLine("It's perfect to go to the beach.");
+                    break;
 
-            Console.WriteLine(price);
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
         }
     }
 }
