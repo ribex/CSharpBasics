0fd9edf54fd0f2770bbc247cc2e85fafcc4e8a93
ribex <ribexy@gmail.com>
2017-11-19 10:33:35 -0500

    Example with string array and inline assignment

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -26,6 +26,11 @@ namespace Arrays
             Console.WriteLine(flags[0]); // True
             Console.WriteLine(flags[1]); // False
             Console.WriteLine(flags[2]); // False
+
+            var names = new string[3] {"Jack", "John", "Mary"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
