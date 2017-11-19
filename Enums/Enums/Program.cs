2a4eab31fc16287e36d4823e9ea31757a605555a
ribex <ribexy@gmail.com>
2017-11-19 11:03:22 -0500

    Examples using Enum class

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        // it's best to explicitly define the values of the enum members
+        // default assignment would start at 0
+        // default data type is int
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            // Console.WriteLine automatically converts to string but ToString method could be used
+            Console.WriteLine(method.ToString());
+
+            // convert string to enum
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
