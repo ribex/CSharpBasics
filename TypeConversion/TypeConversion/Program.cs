0bfcf60e537d28c869fb6f0e96896350bc2487e6
ribex <ribexy@gmail.com>
2017-11-19 09:13:23 -0500

    Example of converting byte to int

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+        }
+
+    }
+}
