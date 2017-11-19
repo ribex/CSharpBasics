466c80484c53440a1caf2c19a1c9e28cecff13c3
ribex <ribexy@gmail.com>
2017-11-19 09:16:16 -0500

    Example of casting int to byte and data is lost (1000 -> 232)

--- TypeConversion/TypeConversion/Program.cs
+++ TypeConversion/TypeConversion/Program.cs
@@ -10,7 +10,7 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            int i = 1;
+            int i = 1000;
             byte b = (byte)i;
             Console.WriteLine(b);
         }
