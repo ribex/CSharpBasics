a7ae5a35f596967f7e23e8f85e1ca4df7f5f7632
ribex <ribexy@gmail.com>
2017-11-19 09:45:32 -0500

    Example of comparison expression yielding a Boolean

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -12,9 +12,8 @@ namespace Operators
         {
             var a = 1;
             var b = 2;
-            var c = 3;
 
-            Console.WriteLine((a + b) * c);
+            Console.WriteLine(a > b);
         }
     }
 }
