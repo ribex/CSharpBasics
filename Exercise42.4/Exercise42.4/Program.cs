0bac1f315f513af0018d1585417dbd7732cdaffd
ribex <ribexy@gmail.com>
2017-11-19 13:10:55 -0500

    Exercise 4: Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. If the user enters a value less than the speed limit, program should display Ok on the console. If the value is above the speed limit, the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended.

---
+++ Exercise42.4/Exercise42.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the speed limit:");
+            var limit = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the car speed:");
+            var speed = Int32.Parse(Console.ReadLine());
+
+            if (speed <= limit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                var demerit = (speed - limit) / 5;
+                if (demerit > 12)
+                {
+                    Console.WriteLine("License Suspended");
+                }
+                else
+                {
+                    Console.WriteLine(demerit);
+                }
+            }
+        }
+    }
+}
