ae5d5661ff3602d6b66eeb516a7c2d80b449511c
ribex <ribexy@gmail.com>
2017-11-19 14:56:56 -0500

    Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.

---
+++ Exercise48.3/Exercise48.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise48._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var factorial = 1;
+            for (var i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
+    }
+}
