c8829a051d5d8d340e67e7c2f73e8f317a0424f8
ribex <ribexy@gmail.com>
2017-11-19 11:55:42 -0500

    Examples of value type (int) and reference type (array)

---
+++ ReferenceTypesAndValueTypes/ReferenceTypesAndValueTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypesAndValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a; // value of a copied to b
+            b++; // a remains 10
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1; // array2 references same memory location as the array stored in array1
+            array2[0] = 0; // changes value in the memory location referenced by both array2 and array1
+            Console.WriteLine(array1[0]); // 0
+            Console.WriteLine("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]); // 0, 0
+        }
+    }
+}
