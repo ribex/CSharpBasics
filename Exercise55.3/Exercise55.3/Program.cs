afa79e4c742556f2af237fc7dc59bd305d42e631
ribex <ribexy@gmail.com>
2017-11-19 20:16:49 -0500

    Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.

---
+++ Exercise55.3/Exercise55.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise55._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter five unique numbers, one per line.");
+            var numbers = new int[5];
+            var count = 0;
+            do
+            {
+                var entry = Convert.ToInt32(Console.ReadLine());
+                var index = Array.IndexOf(numbers, entry);
+                if (index < 0)
+                {
+                    numbers[count] = entry;
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Duplicate number, please retry your entry.");
+                }
+            } while (count < 5);
+
+            // Array.Sort(numbers);
+            for (var i = 0; i < 5; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
