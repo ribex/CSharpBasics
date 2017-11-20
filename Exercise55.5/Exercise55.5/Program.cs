e40133a3046fdc6b03e1e11158253277478eb31c
ribex <ribexy@gmail.com>
2017-11-19 21:33:44 -0500

    Exercise 5:  Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.

---
+++ Exercise55.5/Exercise55.5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise55._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a list of at least 5 numbers separated by commas: ");
+            var numberList = new List<int>();
+
+            do
+            {
+                var entry = Console.ReadLine();
+                var numbers = entry.Split(',');
+
+                if (numbers.Length < 5)
+                {
+                    Console.WriteLine("Invalid List");
+                    Console.WriteLine("Please retry your list: ");
+                    continue;
+                }
+
+                foreach (var number in numbers)
+                {
+                    numberList.Add(Convert.ToInt32(number));
+                }
+
+            } while (numberList.Count < 5);
+
+            numberList.Sort();
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+        }
+    }
+}
