d0a483171a1332c4c10e27d94ca3eb09247cdf1d
ribex <ribexy@gmail.com>
2017-11-20 19:26:41 -0500

    Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.

---
+++ Exercise67.2/Exercise67.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise67._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a few numbers separated by hyphens (-): ");
+
+            var entry = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(entry))
+            {
+                return;
+            }
+
+            var numbers = entry.Split('-');
+
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    Console.WriteLine("Duplicate");
+                }
+            }
+        }
+    }
+}
