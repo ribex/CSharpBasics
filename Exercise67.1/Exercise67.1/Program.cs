cc9e8d28cc03bf5082d75204bba63379e7931289
ribex <ribexy@gmail.com>
2017-11-20 19:05:43 -0500

    Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".

---
+++ Exercise67.1/Exercise67.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise67._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a few numbers separated by hyphens (-): ");
+            string entry = Console.ReadLine();
+            var values = entry.Split('-');
+            var consecutive = true;
+
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                if (Math.Abs(Convert.ToInt32(values[i]) - Convert.ToInt32(values[i + 1])) != 1)
+                {
+                    Console.WriteLine("Not Consecutive");
+                    consecutive = false;
+                    break;
+                }
+            }
+            if (consecutive)
+            {
+                Console.WriteLine("Consecutive");
+            }
+        }
+    }
+}
