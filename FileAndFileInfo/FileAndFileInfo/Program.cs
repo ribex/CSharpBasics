a1797b4cf724c9f7f7412d8409c6d958128a595c
ribex <ribexy@gmail.com>
2017-11-21 19:35:51 -0500

    Examples using FIle and FileInfo

---
+++ FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            // File provides static methods
+            // security check each time
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            // FileInfo provides instance methods
+            // requires creating a FileInfo object
+            // security check only once for each new FileInfo object
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+        }
+    }
+}
