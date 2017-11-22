1089f576074d00678c75aef11a04e875d4de8379
ribex <ribexy@gmail.com>
2017-11-21 20:40:30 -0500

    Examples of Path class usage

---
+++ PathDemo/PathDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            // low level way
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            // better, smarter way!
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
