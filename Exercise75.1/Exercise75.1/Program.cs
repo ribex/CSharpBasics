cba74b238898725446b56779e67011f9e42ae636
ribex <ribexy@gmail.com>
2017-11-23 20:52:08 -0500

    Write a program that reads a text file and displays the number of words.

---
+++ Exercise75.1/Exercise75.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise75._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\test\testfile.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists!");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist.");
+            }
+            var content = File.ReadAllText(path);
+
+            var words = content.Split(' ');
+            var wordCount = words.Count();
+
+            Console.WriteLine("The file contains " + wordCount + " words.");
+        }
+    }
+}
